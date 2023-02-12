9525d6a3c4e20c363c47dbae6f570daed2046297
lilcojje <rodz.painagan@remotestaff.com>
2023-02-12 13:52:57 +0800

    New added

---
+++ src/views/CorporateApps/Timesheet/TheNavbar.vue
@@ -0,0 +1,53 @@
+<template>
+    <nav-header-bar light :headerName='headerTitle'></nav-header-bar>
+</template>
+<script>
+    import NavHeaderBar from "@/views/NavigationBar/TheHeader.vue";
+
+    export default {
+        components: {
+            NavHeaderBar
+        },
+        data() {
+            return {
+                headerTitle: ''
+            }
+        },
+        watch: {
+            $route (to) {
+                this.setHeaderTitle(to);
+            }
+        },
+        mounted() {
+            this.setHeaderTitle(this.$route);
+        },
+        methods: {
+            setHeaderTitle(to) {
+                switch (to.params.slug) {
+                    case 'landing-page':
+                        this.headerTitle = 'Operations Portal'
+                        break;
+                    case 'time-sheet':
+                        this.headerTitle = 'Timesheet Adjustment'
+                        break;
+                
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+</script>
+>
+<style scoped>
+    svg.bi {
+        color: #000;
+    }
+    .navbar-dark .navbar-toggler {
+        color: rgb(0 0 0 / 55%);
+        /* color: #022416 !important; */
+    }
+    .boton{
+        background-color: #76FF5B;
+    }
+</style>
